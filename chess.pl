8d66a42d738daa24a259e491d892ea045ed00ed4
tomassantunes <tomasbantunes@gmail.com>
2022-12-28 10:15:40 +0000

    adicionados exemplos de jogos

---
+++ chess.pl
@@ -0,0 +1,14 @@
+% :- initialization(main).
+
+% main :- print("Hello World").
+
+% algebrica :- print("Algebrica").
+
+% descritiva :- print("descritiva").
+
+% postal :- print("postal").
+
+% xadrez(FORMAT, ACTION) :- print(FORMAT), print(ACTION).
+
+% xadrez(algebrica, X) :- algebrica.
+
