2c36b8c03d3dc79d3b1888d7b9fda876f83f4761
tomassantunes <tomasbantunes@gmail.com>
2022-12-29 23:19:50 +0000

    nada mas muito

--- chess.pl
+++ chess.pl
@@ -1,3 +1,10 @@
 :- initialization(main).
 
-main :- print("Hello World").
+% usar o predicado argument_list(LISTA) para ver os argumentos dados
+% e direcionar para o formato pretendido as acções
+main :- argument_list([FORMAT, ACTION]), print(FORMAT), print(ACTION). 
+
+gets(S) :- get0(C), gets([], C, S).
+gets(S, 10, S).		% 10 é o newline
+gets(S, -1, S).		% -1 é o end-of-file
+gets(I, C, [C|O]) :- get0(CC), gets(I, CC, O).
