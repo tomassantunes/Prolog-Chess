8591d380c33cdd06e4f316ef888f0ce3d093b265
tomassantunes <tomasbantunes@gmail.com>
2022-12-30 19:54:52 +0000

    load, read, unload file

--- chess.pl
+++ chess.pl
@@ -2,9 +2,13 @@
 
 % usar o predicado argument_list(LISTA) para ver os argumentos dados
 % e direcionar para o formato pretendido as acções
-main :- argument_list([FORMAT, ACTION]), print(FORMAT), print(ACTION). 
+main :- argument_list([FORMAT, ACTION]), print(FORMAT), see(ACTION). 
 
 gets(S) :- get0(C), gets([], C, S).
 gets(S, 10, S).		% 10 é o newline
 gets(S, -1, S).		% -1 é o end-of-file
 gets(I, C, [C|O]) :- get0(CC), gets(I, CC, O).
+
+read_file(X) :- gets(X), format("~s", [X]).
+
+unload_file(FILENAME) :- seeing(FILENAME), seen.
